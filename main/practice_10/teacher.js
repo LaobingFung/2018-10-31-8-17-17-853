62c37610489020ac2e130d176f40c2c353cd52f3
LaoBing_Fung <FLB_B401@163.com>
2018-12-23 18:27:26 +0800

    introduce Teacher with Classes

--- main/practice_10/teacher.js
+++ main/practice_10/teacher.js
@@ -1,1 +1,20 @@
-
+"use strict";
+import Person from "../practice_10/person.js"
+class Teacher extends Person {
+  constructor(id, name, age, klasses) {
+    super(id, name, age);
+    this.klasses = klasses;
+  }
+  introduce() {
+    return `${super.introduce()} I am a Teacher. I teach ${this.klasses? `Class ${this.klasses.map((curklass) => curklass.number).join(', ')}` : `No Class`}.`;
+  }
+  isTeaching(student) {
+    for (let klass of klasses) {
+      if (klass.isIn(student)) {
+        return true;
+      }
+    }
+    return false;
+  }
+}
+module.exports = Teacher;
