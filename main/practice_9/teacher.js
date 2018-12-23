a6efaa1aca83c9d03c1fab4398ed2a2a7d1ffe6f
LaoBing_Fung <FLB_B401@163.com>
2018-12-23 17:42:41 +0800

    append new member to Class

--- main/practice_9/teacher.js
+++ main/practice_9/teacher.js
@@ -1,1 +1,12 @@
-
+"use strict";
+import Person from "../practice_9/person.js"
+class Teacher extends Person {
+  constructor(id, name, age, klass) {
+    super(id, name, age);
+    this.klass = klass;
+  }
+  introduce() {
+    return `${super.introduce()} I am a Teacher. I teach ${this.klass? this.klass.getDisplayName() : `No Class`}.`;
+  }
+}
+module.exports = Teacher;
