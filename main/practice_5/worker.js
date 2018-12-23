aa3fa04031339843508ba9e68cf32ed11ebfa8e9
LaoBing_Fung <FLB_B401@163.com>
2018-12-23 15:44:23 +0800

    call function of basic class in subclass

--- main/practice_5/worker.js
+++ main/practice_5/worker.js
@@ -1,1 +1,11 @@
-
+"use strict";
+import Person from "../practice_5/person.js";
+class Worker extends Person {
+  constructor(name, age) {
+    super(name, age);
+  }
+  introduce() {
+    return `${super.introduce()} I am a Teacher. I have a job.`;
+  }
+}
+module.exports = Worker;
