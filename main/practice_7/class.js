ee4044c845cd554a2ecc1f84f79828c879265c3b
LaoBing_Fung <FLB_B401@163.com>
2018-12-23 16:51:42 +0800

    introduce teacher with student

--- main/practice_7/class.js
+++ main/practice_7/class.js
@@ -1,1 +1,10 @@
-
+"use strict";
+class Class {
+  constructor(number) {
+    this.number = number;
+  }
+  getDisplayName() {
+    return `Class ${this.number}`;
+  }
+}
+module.exports = Class;
--- main/practice_7/student.js
+++ main/practice_7/student.js
@@ -1,1 +1,12 @@
-
+"use strict";
+import Person from "../practice_7/Person.js";
+class Student extends Person {
+  constructor(name, age, klass) {
+    super(name, age);
+    this.klass = klass;
+  }
+  introduce() {
+    return `${super.introduce()} I am a Student. I am at ${this.klass.getDisplayName()}.`
+  }
+}
+module.exports = Student;
