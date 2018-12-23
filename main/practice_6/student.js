2f4fc569744250cf646e9900afc171ffc1c758ef
LaoBing_Fung <FLB_B401@163.com>
2018-12-23 16:03:39 +0800

    create Teacher class by inheriting Person class

--- main/practice_6/student.js
+++ main/practice_6/student.js
@@ -1,1 +1,12 @@
-
+"use strict";
+import Person from "../practice_6/Person.js";
+class Student extends Person {
+  constructor(name, age, klass) {
+    super(name, age);
+    this.klass = klass;
+  }
+  introduce() {
+    return `${super.introduce()} I am a Student. I am at Class ${this.klass}.`
+  }
+}
+module.exports = Student;
--- main/practice_6/teacher.js
+++ main/practice_6/teacher.js
@@ -1,1 +1,12 @@
-
+"use strict";
+import Person from "../practice_5/person.js";
+class Teacher extends Person {
+  constructor(name, age, klass) {
+    super(name, age);
+    this.klass = klass;
+  }
+  introduce() {
+    return `${super.introduce()} I am a Teacher. I teach ${this.klass? `Class ${this.klass}` : `No Class`}.`;
+  }
+}
+module.exports = Teacher;
